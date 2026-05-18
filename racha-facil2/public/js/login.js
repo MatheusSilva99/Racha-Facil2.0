8f991b1ee1228a06349ca3b4d3f18aab89d3ca0b
MatheusSilva99 <matheusbars321@gmail.com>
2026-05-18 17:58:01 +0000

    feat: implementa login.js para registro de email e senha

---
+++ racha-facil2/public/js/login.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, senha })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        alert("Login realizado com sucesso!");
+
+        window.location.href = "/divisao.html";
+    } else {
+        alert(data.erro);
+    }
+});
